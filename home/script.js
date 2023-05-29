9e168cc780bdb74817d7c8348b299d003a0ec18d
“NicholasAntonio” <nicholas.nkt.antonio@gmail.com>
2023-05-29 00:30:15 -0300

    add mobile menu and responsivity

--- home/script.js
+++ home/script.js
@@ -22,3 +22,18 @@ window.addEventListener('scroll', () =>{
     }
 }
 )
+
+const hamburguer = document.getElementById('mobile-menu');
+const icon = document.querySelector('.fa-bars');
+const logo = document.querySelector('logo');
+
+hamburguer.onclick = function(){
+   const navBar = document.querySelector('.nav-links');
+   navBar.classList.toggle('active');
+   icon.classList.toggle('fa-xmark');
+   if (nav.classList.contains('navbarBlack')){
+    nav.classList.remove('navbarBlack');
+   }else{
+    nav.classList.add('navbarBlack');
+   }
+}
