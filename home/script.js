8d309daea5379a4d8e7a474fc071d95a954a7128
“NicholasAntonio” <nicholas.nkt.antonio@gmail.com>
2023-06-08 17:03:49 -0300

    site commit

--- home/script.js
+++ home/script.js
@@ -4,36 +4,12 @@ const nav = document.getElementById('navbar');
 
 setInterval(function(){
     next()
-},8000)
+},8000);
 
 function next(){
     count ++;
     if(count>3){
         count = 1;
     }
-    document.getElementById("radio" + count).checked = true
-}
-
-window.addEventListener('scroll', () =>{
-    if(window.scrollY >= 100){
-        nav.classList.add('navbarBlack');
-    }else{
-        nav.classList.remove('navbarBlack');
-    }
-}
-)
-
-const hamburguer = document.getElementById('mobile-menu');
-const icon = document.querySelector('.fa-bars');
-const logo = document.querySelector('logo');
-
-hamburguer.onclick = function(){
-   const navBar = document.querySelector('.nav-links');
-   navBar.classList.toggle('active');
-   icon.classList.toggle('fa-xmark');
-   if (nav.classList.contains('navbarBlack')){
-    nav.classList.remove('navbarBlack');
-   }else{
-    nav.classList.add('navbarBlack');
-   }
-}
+    document.getElementById("radio" + count).checked = true;
+};
--- js/generalscript.js
+++ js/generalscript.js
@@ -1,9 +1,9 @@
 const hamburguer = document.getElementById('mobile-menu');
 const icon = document.querySelector('.fa-bars');
-const logo = document.querySelector('logo');
 
 hamburguer.onclick = function(){
    const navBar = document.querySelector('.nav-links');
    navBar.classList.toggle('active');
    icon.classList.toggle('fa-xmark');
-}
+};
+
